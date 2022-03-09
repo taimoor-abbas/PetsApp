cdfbc624b7edfd6600abe2853eb38bfb84396b99
Taimoor ABBAS <taimoorabbas201@gmail.com>
2022-03-09 11:32:56 -0800

    Pets

---
+++ app/src/main/java/com/example/pets/CatalogActivity.java
@@ -0,0 +1,132 @@
+package com.example.pets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.pets.data.PetContract.PetEntry;
+import com.example.pets.data.PetDbHelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+public class CatalogActivity extends AppCompatActivity {
+private  PetDbHelper mDbHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mDbHelper=new PetDbHelper(this);
+
+    }
+
+    /**
+     * Temporary helper method to display information in the onscreen TextView about the state of
+     * the pets database.
+     */
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        displayDatabaseInfo();
+    }
+    private void displayDatabaseInfo() {
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        mDbHelper = new PetDbHelper(this);
+/*
+  String[] projection={
+  PetEntry._ID,
+  PetEntry.COLUMN_PET_NAME,
+  PetEntry.COLUMN_PET_BREED,
+  PetEntry.COLUMN_PET_WEIGHT,
+  PetEntry.COLUMN_PET_GENDER,
+};
+Cursor  cursor= db.query(
+        PetEntry.TABLE_NAME,
+        projection,
+        null,
+        null,
+        null,
+        null,
+        null);
+        // Perform this raw SQL query "SELECT * FROM pets"
+        // to get a Cursor that contains all rows from the pets table.
+     */
+        Cursor cursor=db.rawQuery("select * FROM "+PetEntry.TABLE_NAME,null);
+        try {
+            // Display the number of rows in the Cursor (which reflects the number of rows in the
+            // pets table in the database).
+            TextView displayView = (TextView) findViewById(R.id.text_view_Pet);
+            displayView.setText("Number of rows in pets database table: " + cursor.getCount());
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
+    }
+
+
+
+    private  void  insertPet()
+{
+SQLiteDatabase db=mDbHelper.getWritableDatabase();
+    ContentValues values=new ContentValues();
+    values.put(PetEntry.COLUMN_PET_NAME,"TOTO");
+    values.put(PetEntry.COLUMN_PET_BREED,"Terrier");
+    values.put(PetEntry.COLUMN_PET_GENDER,PetEntry.GENDER_MALE);
+    values.put(PetEntry.COLUMN_PET_WEIGHT,7);
+    long newRodId=db.insert(PetEntry.TABLE_NAME,null,values);
+    Log.v("CatalogActivity","New row ID"+newRodId);
+}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                // Do nothing for now
+                insertPet();
+                displayDatabaseInfo();
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                // Do nothing for now
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
+
+
